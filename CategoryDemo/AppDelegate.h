fa8d6ef2ded7772479d2d7ce5c023693c1280cd1
Dacilia <1257249648@qq.com>
2018-04-08 11:42:12 +0800

    Initial Commit

---
+++ CategoryDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CategoryDemo
+//
+//  Created by 李达志 on 2018/4/8.
+//  Copyright © 2018年 LDZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
